1625702eab3cba329ba32905bdd93d4e8b85f28b
Alex Citterio <alex.citterio@ipsos.com>
2024-05-14 12:38:56 +0100

    add -dev and -tests as reqiurements.txt in web devicon

--- lua/plugins/nvim-web-devicons.lua
+++ lua/plugins/nvim-web-devicons.lua
@@ -16,6 +16,17 @@ M.config = function ()
                 color = "#2D4F67",
                 name = "Requirements"
             },
+            ["requirements-dev.txt"] = {
+                icon = "",
+                color = "#2D4F67",
+                name = "Requirements"
+            },
+            ["requirements-tests.txt"] = {
+                icon = "",
+                color = "#2D4F67",
+                name = "Requirements"
+            },
+
         }
     }
 end
