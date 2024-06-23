55c50f8bd3d17a51e5f58b62375f9cb48ecce037
the-citto <alex.citterio@hotmail.co.uk>
2024-06-23 16:24:28 +0100

    add telescope keybinds

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -15,15 +15,25 @@ local opts = {}
 
 local mappings = {
     ["<leader>"] = {
-        b = {
-            '<cmd>lua require"telescope.builtin".buffers(require"telescope.themes".get_dropdown{previewer=false})<cr>',
-            "Buffers",
-        },
-        f = {
+        b = { "<cmd>Telescope buffers<cr>", ":Telescope buffers", },
+        -- b = {
+        --     '<cmd>lua require"telescope.builtin".buffers(require"telescope.themes".get_dropdown{previewer=false})<cr>',
+        --     "Buffers",
+        -- },
+        s = {
             name = "Telescope",
+            c = { "<cmd>Telescope commands<cr>", ":Telescope commands", },
             f = { "<cmd>Telescope find_files<cr>", ":Telescope find_files", },
-            g = { "<cmd>Telescope git_files<cr>", ":Telescope git_files", },
+            g = {
+                name = "git",
+                b = { "<cmd>Telescope git_branches<cr>", ":Telescope git_branches", },
+                c = { "<cmd>Telescope git_commit<cr>", ":Telescope git_commit", },
+                f = { "<cmd>Telescope git_files<cr>", ":Telescope git_files", },
+                s = { "<cmd>Telescope git_status<cr>", ":Telescope git_status", },
+            },
+            k = { "<cmd>Telescope keymaps<cr>", ":Telescope keymaps", },
             l = { "<cmd>Telescope live_grep<cr>", ":Telescope live_grep", },
+            m = { "<cmd>Telescope marks<cr>", ":Telescope marks", },
             o = { "<cmd>Telescope oldfiles<cr>", ":Telescope oldfiles", },
             -- s = { "<cmd>Telescope software-licenses find<cr>", ":Telescope software-licenses find", },
         },
