2dd64a2a5b58088ba2877558bdbc4e8222930acc
the-citto <alex.citterio@hotmail.co.uk>
2024-07-21 09:27:24 +0100

    update plugins

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -57,6 +57,68 @@ local mappings = {
     },
 }
 
+-- local mappings = {
+--     { "<leader>b", "<cmd>Telescope buffers<cr>", desc = ":Telescope buffers" },
+--     { "<leader>f", "<cmd>Telescope find_files<cr>", desc = ":Telescope find_files" },
+--     { "<leader>h", "<cmd>Hardtime toggle<cr>", desc = ":Hardtime toggle" },
+--
+--     { "<leader>q", group = "quickfix" },
+--     { "<leader>qc", "<cmd>cclose<cr>", desc = ":cclose" },
+--     { "<leader>qo", "<cmd>copen<cr>", desc = ":copen" },
+--     { "<leader>qr", "<cmd>call setqflist([])<cr><cmd>cclose<cr>", desc = "reset quickfix list" },
+--
+--     { "<leader>s", group = "Telescope" },
+--     { "<leader>sc", "<cmd>Telescope commands<cr>", desc = ":Telescope commands" },
+--     { "<leader>sf", "<cmd>Telescope find_files<cr>", desc = ":Telescope find_files" },
+--
+--     { "<leader>sg", group = "git" },
+--     { "<leader>sgb", "<cmd>Telescope git_branches<cr>", desc = ":Telescope git_branches" },
+--     { "<leader>sgc", "<cmd>Telescope git_commit<cr>", desc = ":Telescope git_commit" },
+--     { "<leader>sgf", "<cmd>Telescope git_files<cr>", desc = ":Telescope git_files" },
+--     { "<leader>sgs", "<cmd>Telescope git_status<cr>", desc = ":Telescope git_status" },
+--     { "<leader>sk", "<cmd>Telescope keymaps<cr>", desc = ":Telescope keymaps" },
+--     { "<leader>sl", "<cmd>Telescope live_grep<cr>", desc = ":Telescope live_grep" },
+--     { "<leader>sm", "<cmd>Telescope marks<cr>", desc = ":Telescope marks" },
+--     { "<leader>so", "<cmd>Telescope oldfiles<cr>", desc = ":Telescope oldfiles" },
+--     { "<leader>sq", "<cmd>Telescope quickfix<cr>", desc = ":Telescope quickfix" },
+--
+--     { "<leader>t", group = "Trouble" },
+--     { "<leader>tf", "<cmd>Trouble diagnostics focus<cr>", desc = ":Trouble diagnostics focus" },
+--     { "<leader>tt", "<cmd>Trouble diagnostics toggle<cr>", desc = ":Trouble diagnostics toggle" },
+-- }
+
+-- local mappings =   {
+--     { "", "<leader>b", desc = "<cmd>Telescope buffers<cr>" },
+--     { "", "<leader>sk", desc = "<cmd>Telescope keymaps<cr>" },
+--     { "", "<leader>sgs", desc = "<cmd>Telescope git_status<cr>" },
+--     { "", "<leader>sgf", desc = "<cmd>Telescope git_files<cr>" },
+--     { "", "<leader>sgc", desc = "<cmd>Telescope git_commit<cr>" },
+--     { "", "<leader>sl", desc = "<cmd>Telescope live_grep<cr>" },
+--     { "", "<leader>so", desc = "<cmd>Telescope oldfiles<cr>" },
+--     { "", "<leader>sm", desc = "<cmd>Telescope marks<cr>" },
+--     { "", group = "Trouble" },
+--     { "", "<leader>sq", desc = "<cmd>Telescope quickfix<cr>" },
+--     { "", "<leader>tf", desc = "<cmd>Trouble diagnostics focus<cr>" },
+--     { "", "<leader>sgb", desc = "<cmd>Telescope git_branches<cr>" },
+--     { "", "<leader>sf", desc = "<cmd>Telescope find_files<cr>" },
+--     { "", group = "quickfix" },
+--     { "", "<leader>h", desc = "<cmd>Hardtime toggle<cr>" },
+--     { "", "<leader>f", desc = "<cmd>Telescope find_files<cr>" },
+--     { "", group = "git" },
+--     { "", "<leader>qc", desc = "<cmd>cclose<cr>" },
+--     { "", "<leader>qr", desc = "<cmd>call setqflist([])<cr><cmd>cclose<cr>" },
+--     { "", "<leader>qo", desc = "<cmd>copen<cr>" },
+--     { "", "<leader>sc", desc = "<cmd>Telescope commands<cr>" },
+--     { "", group = "Telescope" },
+--     { "", "<leader>tt", desc = "<cmd>Trouble diagnostics toggle<cr>" },
+--   }
+-- - WARNING overwriting desc: >
+--   { "<leader>b", "<cmd>Telescope buffers<cr>",
+--     desc = ":Telescope buffers",
+--     prefix = ""
+--   }
+
+
 M.config = function()
     require("which-key").register(mappings, opts)
 end
