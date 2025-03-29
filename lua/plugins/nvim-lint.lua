c7091bc9c734789894adbb90a5f36672eb57f30b
the-citto <alex.citterio@hotmail.co.uk>
2025-03-29 09:42:03 +0000

    start cleanup

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -43,7 +43,7 @@ M.config = function()
     --     ),
     --     -- cmd = ".venv/bin/mypy"
     -- }
-    -- lint.linters.mypy.cmd = ".venv/bin/mypy"
+    -- lint.linters.mypy.cmd = "/home/lexxx/.local/share/nvim/mason/packages/mypy/venv/bin/mypy"
     -- lint.linters.mypy.stdin = true
     -- lint.linters.mypy.args = {
     --     '--show-column-numbers',
@@ -61,11 +61,11 @@ M.config = function()
         -- typescript = { "eslint_d" },
         -- typescriptreact = { "eslint_d" },
     }
-    local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+    -- local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
     vim.api.nvim_create_autocmd(
         {"BufEnter", "BufWritePost", "InsertLeave"},
         {
-            group = lint_augroup,
+            -- group = lint_augroup,
             callback = function()
                 lint.try_lint()
                 -- lint.try_lint("mypy")
