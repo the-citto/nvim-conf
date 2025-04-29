cee6c3a2c1de3f763425bfbd7876d2df5a86be40
the-citto <alex.citterio@hotmail.co.uk>
2025-04-29 07:08:58 +0100

    add conform

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,22 @@
+
+local M = {
+  'stevearc/conform.nvim',
+}
+
+
+
+M.config = function()
+    require("conform").setup({
+        formatters_by_ft = {
+            -- lua = { "stylua" },
+            python = { "isort", "black" },
+            -- rust = { "rustfmt", lsp_format = "fallback" },
+            -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        },
+    })
+end
+
+
+return M
+
+
---
+++ lua/plugins/mason-conform.lua
@@ -0,0 +1,18 @@
+
+
+
+local M = {
+    "zapling/mason-conform.nvim",
+    dependencies = {
+        "williamboman/mason.nvim",
+        "stevearc/conform.nvim",
+    },
+}
+
+M.config = function()
+
+end
+
+return M
+
+
