459e32f52e5bbdba3070f695cdbd80bdeeb0a265
the-citto <alex.citterio@hotmail.co.uk>
2025-10-19 20:24:06 +0100

    improve lint and fmt

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -9,10 +9,20 @@ M.config = function()
     require("conform").setup({
         formatters_by_ft = {
             lua = { "stylua" },
-            python = { "isort", "black" },
-            -- rust = { "rustfmt", lsp_format = "fallback" },
+            python = {
+                "isort",
+                "black",
+                "ruff",
+            },
+            rust = { "rustfmt", lsp_format = "fallback" },
             -- javascript = { "prettierd", "prettier", stop_after_first = true },
         },
+    })
+    vim.api.nvim_create_autocmd("BufWritePre", {
+        pattern = "*",
+        callback = function(args)
+            require("conform").format({ bufnr = args.buf })
+        end,
     })
 end
 
--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -37,6 +37,16 @@ M.config = function()
             end,
         }
     )
+    vim.api.nvim_create_user_command("LintInfo", function()
+        local filetype = vim.bo.filetype
+        local linters = require("lint").linters_by_ft[filetype]
+
+        if linters then
+            print("Linters for " .. filetype .. ": " .. table.concat(linters, ", "))
+        else
+            print("No linters configured for filetype: " .. filetype)
+        end
+    end, {})
 end
 
 return M
