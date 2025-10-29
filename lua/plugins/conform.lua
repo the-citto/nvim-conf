2474ad76d2119879b2592b62568a2f022db321bf
the-citto <alex.citterio@hotmail.co.uk>
2025-10-29 16:08:57 +0000

    fix: text to repl

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -8,7 +8,10 @@ local M = {
 M.config = function()
     require("conform").setup({
         formatters_by_ft = {
-            lua = { "stylua" },
+            lua = {
+                "stylua",
+                -- "luaformatter",
+            },
             python = {
                 "isort",
                 "black",
