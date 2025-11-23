dd3033fce489a989387b781483f8583bb68b1901
the-citto <alex.citterio@hotmail.co.uk>
2025-11-23 18:20:19 +0000

    dev: change win ty

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -10,6 +10,18 @@ local M = {
 
 -- REMEMBER: update Mason lspcinfig
 
+local ty_path = "ty"
+local _, linux_path = next(vim.fs.find({"ty"}, {type = "file", path = "./.venv/bin"}))
+if linux_path ~= nil then
+    ty_path = linux_path
+    -- mypy_path = vim.fn.getcwd() .. "/" .. linux_path
+end
+local _, win_path = next(vim.fs.find({"ty.exe"}, {type = "file", path = "./.venv/Scripts"}))
+if win_path ~= nil then
+    ty_path = win_path
+end
+
+
 M.config = function()
     local capabilities = require('blink.cmp').get_lsp_capabilities()
     vim.lsp.config("clangd", { capabilities = capabilities })
@@ -45,7 +57,12 @@ M.config = function()
     vim.lsp.config("terraformls", { capabilities = capabilities })
     vim.lsp.config("tflint", { capabilities = capabilities })
     vim.lsp.config("ts_ls", { capabilities = capabilities })
-    vim.lsp.config("ty", { capabilities = capabilities })
+    vim.lsp.config("ty", {
+        capabilities = capabilities,
+        settings = {
+            cmd = {ty_path, "server" },
+        },
+    })
     vim.lsp.config("yamlls", { capabilities = capabilities })
     vim.diagnostic.config{
         virtual_text = true,
