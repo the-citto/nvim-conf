34d6093718a1cc0b471389490e477d4ddc0b1b7a
the-citto <alex.citterio@hotmail.co.uk>
2025-11-23 17:52:43 +0000

    dev: change win mypy

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -12,7 +12,8 @@ if linux_path ~= nil then
 end
 local _, win_path = next(vim.fs.find({"mypy.exe"}, {type = "file", path = "./.venv/Scripts"}))
 if win_path ~= nil then
-    mypy_path = win_path
+    -- mypy_path = win_path
+    mypy_path = "uv.exe run mypy"
 end
 
 
