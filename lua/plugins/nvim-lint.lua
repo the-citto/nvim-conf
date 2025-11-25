d7ad51ef18790c634d610aead53a2ee7454e1bb2
the-citto <alex.citterio@hotmail.co.uk>
2025-11-25 09:15:26 +0000

    lint - simple mypy

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -15,11 +15,11 @@ local M = {
 --     mypy_path = win_path
 -- end
 
-local mypy_cmd = "mypy"
-local _, win_python = next(vim.fs.find({"python.exe"}, {type = "file", path = "./.venv/Scripts"}))
-if win_python ~= nil then
-    mypy_cmd = mypy_cmd .. " --python-executable " .. win_python
-end
+-- local mypy_cmd = "mypy"
+-- local _, win_python = next(vim.fs.find({"python.exe"}, {type = "file", path = "./.venv/Scripts"}))
+-- if win_python ~= nil then
+--     mypy_cmd = mypy_cmd .. " --python-executable " .. win_python
+-- end
 
 M.config = function()
     local lint = require("lint")
@@ -34,7 +34,7 @@ M.config = function()
         -- },
     }
     -- lint.linters.mypy.cmd = mypy_path
-    lint.linters.mypy.cmd = mypy_cmd
+    -- lint.linters.mypy.cmd = mypy_cmd
     vim.api.nvim_create_autocmd(
         {"BufEnter", "BufWritePost", "InsertLeave"},
         {
