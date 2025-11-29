52cc6f94aa81faf3dd8104b5027fbf84a16e8b92
the-citto <alex.citterio@hotmail.co.uk>
2025-11-29 17:56:50 +0000

    set conform for wsl-win

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -2,6 +2,33 @@ local M = {
 	"stevearc/conform.nvim",
 }
 
+local python_formatters = function()
+	if vim.fn.getenv("IS_WSL_WIN") == "1" then
+		return {
+			"isort",
+			"black",
+			"ruff",
+		}
+	else
+		return {
+			black = {
+				command = "pwsh.exe",
+				args = {
+					"-Command",
+					"uv run black --stdin-filename $FILENAME --quiet -",
+				},
+			},
+			ruff = {
+				command = "pwsh.exe",
+				args = {
+					"-Command",
+					"uv run ruff check --fix --force-exclude --exit-zero --no-cache --stdin-filename $FILENAME -",
+				},
+			},
+		}
+	end
+end
+
 M.config = function()
 	local conform = require("conform")
 	conform.setup({
@@ -10,40 +37,36 @@ M.config = function()
 				"stylua",
 				-- "luaformatter",
 			},
-			python = {
-				"isort",
-				"black",
-				"ruff",
-			},
+			python = python_formatters(),
 			rust = { "rustfmt", lsp_format = "fallback" },
 			-- javascript = { "prettierd", "prettier", stop_after_first = true },
 		},
 	})
-	if vim.fn.getenv("IS_WSL_WIN") == "1" then
-		conform.formatters.ruff = {
-			-- inherit = false,
-			command = "pwsh.exe",
-			args = {
-				"-Command",
-				"uv run ruff check --fix --force-exclude --exit-zero --no-cache --stdin-filename $FILENAME -",
-			},
-		}
-		conform.formatters.black = {
-			-- inherit = false,
-			command = "pwsh.exe",
-			args = {
-				"-Command",
-				"uv run black --stdin-filename $FILENAME --quiet -",
-			},
-		}
-		conform.formatters.isort = {
-			-- inherit = false,
-			command = "",
-			args = {
-				"",
-			},
-		}
-	end
+	-- if vim.fn.getenv("IS_WSL_WIN") == "1" then
+	-- 	conform.formatters.ruff = {
+	-- 		-- inherit = false,
+	-- 		command = "pwsh.exe",
+	-- 		args = {
+	-- 			"-Command",
+	-- 			"uv run ruff check --fix --force-exclude --exit-zero --no-cache --stdin-filename $FILENAME -",
+	-- 		},
+	-- 	}
+	-- 	conform.formatters.black = {
+	-- 		-- inherit = false,
+	-- 		command = "pwsh.exe",
+	-- 		args = {
+	-- 			"-Command",
+	-- 			"uv run black --stdin-filename $FILENAME --quiet -",
+	-- 		},
+	-- 	}
+	-- 	conform.formatters.isort = {
+	-- 		-- inherit = false,
+	-- 		command = "",
+	-- 		args = {
+	-- 			"",
+	-- 		},
+	-- 	}
+	-- end
 	vim.api.nvim_create_autocmd("BufWritePre", {
 		pattern = "*",
 		callback = function(args)
