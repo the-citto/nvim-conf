92b84e7fb67c4ab7e01f66028e1fd8025f9c7dfe
the-citto <alex.citterio@hotmail.co.uk>
2025-11-29 11:24:28 +0000

    set IS_WSL_WIN env variable

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -1,52 +1,46 @@
-
-
 local M = {
-    "mfussenegger/nvim-lint",
+	"mfussenegger/nvim-lint",
 }
 
-local python_linters = function ()
-    local all = {
-        "flake8",
-        "mypy",
-    }
-    if vim.fn.getcwd():match( "/([^/]+)") == "mnt" then
-        for idx, name in ipairs(all) do
-            if name == "mypy" then
-                table.remove(all, idx)
-            end
-        end
-    end
-    return all
+local python_linters = function()
+	local all = {
+		"flake8",
+		"mypy",
+	}
+	-- if vim.fn.getcwd():match( "/([^/]+)") == "mnt" then
+	if vim.fn.getenv("IS_WSL_WIN") then
+		for idx, name in ipairs(all) do
+			if name == "mypy" then
+				table.remove(all, idx)
+			end
+		end
+	end
+	return all
 end
 
 M.config = function()
-    local lint = require("lint")
-    lint.linters_by_ft = {
-        python = python_linters()
-        -- terraform = {
-        --     "terraform",
-        -- },
-    }
-    vim.api.nvim_create_autocmd(
-        {"BufEnter", "BufWritePost", "InsertLeave"},
-        {
-            callback = function()
-                lint.try_lint()
-            end,
-        }
-    )
-    vim.api.nvim_create_user_command("LintInfo", function()
-        local filetype = vim.bo.filetype
-        local linters = require("lint").linters_by_ft[filetype]
+	local lint = require("lint")
+	lint.linters_by_ft = {
+		python = python_linters(),
+		-- terraform = {
+		--     "terraform",
+		-- },
+	}
+	vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+		callback = function()
+			lint.try_lint()
+		end,
+	})
+	vim.api.nvim_create_user_command("LintInfo", function()
+		local filetype = vim.bo.filetype
+		local linters = require("lint").linters_by_ft[filetype]
 
-        if linters then
-            print("Linters for " .. filetype .. ": " .. table.concat(linters, ", "))
-        else
-            print("No linters configured for filetype: " .. filetype)
-        end
-    end, {})
+		if linters then
+			print("Linters for " .. filetype .. ": " .. table.concat(linters, ", "))
+		else
+			print("No linters configured for filetype: " .. filetype)
+		end
+	end, {})
 end
 
 return M
-
-
--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,82 +1,80 @@
-
-
 local M = {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-        {"williamboman/mason-lspconfig.nvim"},
-        {"saghen/blink.cmp"},
-    },
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		{ "williamboman/mason-lspconfig.nvim" },
+		{ "saghen/blink.cmp" },
+	},
 }
 
 -- REMEMBER: update Mason lspcinfig
 
 M.config = function()
-    local capabilities = require('blink.cmp').get_lsp_capabilities()
-    vim.lsp.config("clangd", { capabilities = capabilities })
-    vim.lsp.config("cssls", { capabilities = capabilities })
-    vim.lsp.config("docker_compose_language_service", { capabilities = capabilities })
-    vim.lsp.config("dockerls", { capabilities = capabilities })
-    vim.lsp.config("gh_actions_ls", { capabilities = capabilities })
-    vim.lsp.config("golangci_lint_ls", { capabilities = capabilities })
-    vim.lsp.config("gopls", { capabilities = capabilities })
-    vim.lsp.config("html", { capabilities = capabilities })
-    vim.lsp.config("htmx", { capabilities = capabilities })
-    vim.lsp.config("jinja_lsp", { capabilities = capabilities })
-    vim.lsp.config("jsonls", { capabilities = capabilities })
-    vim.lsp.config("lua_ls", {
-        capabilities = capabilities,
-        settings = {
-            Lua = {
-                diagnostics = {
-                    disable = {
-                        "missing-fields",
-                    },
-                },
-            },
-        }
-    })
-    vim.lsp.config("marksman", { capabilities = capabilities })
-    vim.lsp.config("pyright", { capabilities = capabilities })
-    vim.lsp.config("ruff", { capabilities = capabilities })
-    vim.lsp.config("rust_analyzer", { capabilities = capabilities })
-    vim.lsp.config("sqlls", { capabilities = capabilities })
-    vim.lsp.config("taplo", { capabilities = capabilities })
-    vim.lsp.config("tailwindcss", { capabilities = capabilities })
-    vim.lsp.config("terraformls", { capabilities = capabilities })
-    vim.lsp.config("tflint", { capabilities = capabilities })
-    vim.lsp.config("ts_ls", { capabilities = capabilities })
-    vim.lsp.config("ty", { capabilities = capabilities })
-    vim.lsp.config("yamlls", { capabilities = capabilities })
-    -- if vim.fn.getcwd():match( "/([^/]+)") == "mnt" then
-    --     vim.lsp.config("ty", { cmd = {} })
-    -- end
-    vim.diagnostic.config{
-        virtual_text = true,
-        underline = false,
-    }
-    local no_jump_servers = {
-        "ruff",
-        "ty",
-    }
-    vim.api.nvim_create_autocmd('LspAttach', {
-        group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-        callback = function(ev)
-            local client = vim.lsp.get_client_by_id(ev.data.client_id)
-            if not client then return end
-            for _, no_jump_server in ipairs(no_jump_servers) do
-                if no_jump_server == client.name then
-                    client.server_capabilities.definitionProvider = false
-                    client.server_capabilities.completionProvider = nil
-                end
-            end
-            vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
-            local buffer = ev.buf
-            vim.keymap.set('n', '<leader>d', vim.lsp.buf.definition, { buffer = buffer, desc = "jump to definition" })
-        end,
-    })
+	local capabilities = require("blink.cmp").get_lsp_capabilities()
+	vim.lsp.config("clangd", { capabilities = capabilities })
+	vim.lsp.config("cssls", { capabilities = capabilities })
+	vim.lsp.config("docker_compose_language_service", { capabilities = capabilities })
+	vim.lsp.config("dockerls", { capabilities = capabilities })
+	vim.lsp.config("gh_actions_ls", { capabilities = capabilities })
+	vim.lsp.config("golangci_lint_ls", { capabilities = capabilities })
+	vim.lsp.config("gopls", { capabilities = capabilities })
+	vim.lsp.config("html", { capabilities = capabilities })
+	vim.lsp.config("htmx", { capabilities = capabilities })
+	vim.lsp.config("jinja_lsp", { capabilities = capabilities })
+	vim.lsp.config("jsonls", { capabilities = capabilities })
+	vim.lsp.config("lua_ls", {
+		capabilities = capabilities,
+		settings = {
+			Lua = {
+				diagnostics = {
+					disable = {
+						"missing-fields",
+					},
+				},
+			},
+		},
+	})
+	vim.lsp.config("marksman", { capabilities = capabilities })
+	vim.lsp.config("pyright", { capabilities = capabilities })
+	vim.lsp.config("ruff", { capabilities = capabilities })
+	vim.lsp.config("rust_analyzer", { capabilities = capabilities })
+	vim.lsp.config("sqlls", { capabilities = capabilities })
+	vim.lsp.config("taplo", { capabilities = capabilities })
+	vim.lsp.config("tailwindcss", { capabilities = capabilities })
+	vim.lsp.config("terraformls", { capabilities = capabilities })
+	vim.lsp.config("tflint", { capabilities = capabilities })
+	vim.lsp.config("ts_ls", { capabilities = capabilities })
+	vim.lsp.config("ty", { capabilities = capabilities })
+	vim.lsp.config("yamlls", { capabilities = capabilities })
+	-- if vim.fn.getcwd():match( "/([^/]+)") == "mnt" then
+	-- if vim.fn.getenv("IS_WSL_WIN") then
+	--     vim.lsp.config("ty", { cmd = {} })
+	-- end
+	vim.diagnostic.config({
+		virtual_text = true,
+		underline = false,
+	})
+	local no_jump_servers = {
+		"ruff",
+		"ty",
+	}
+	vim.api.nvim_create_autocmd("LspAttach", {
+		group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+		callback = function(ev)
+			local client = vim.lsp.get_client_by_id(ev.data.client_id)
+			if not client then
+				return
+			end
+			for _, no_jump_server in ipairs(no_jump_servers) do
+				if no_jump_server == client.name then
+					client.server_capabilities.definitionProvider = false
+					client.server_capabilities.completionProvider = nil
+				end
+			end
+			vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+			local buffer = ev.buf
+			vim.keymap.set("n", "<leader>d", vim.lsp.buf.definition, { buffer = buffer, desc = "jump to definition" })
+		end,
+	})
 end
 
-
 return M
-
-
