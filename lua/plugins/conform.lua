4a8d889ce4b502fbbb8405200b5170d3f51717db
the-citto <alex.citterio@hotmail.co.uk>
2025-11-29 19:59:05 +0000

    set conform for wsl-win

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -4,31 +4,9 @@ local M = {
 
 local python_wsl_win_formatters = function()
 	if vim.fn.getenv("IS_WSL_WIN") == "1" then
-		return {
-			isort = {
-				command = 'pwsh.exe -Command "uv run isort --line-ending lf $FILENAME"',
-				-- args = {
-				-- 	"-Command",
-				-- 	"uv run isort --stdout --line-ending lf --filename $FILENAME -",
-				-- },
-			},
-			black = {
-				command = 'pwsh.exe -Command "uv run black --quiet $FILENAME"',
-				-- 	args = {
-				-- 		"-Command",
-				-- 		"uv run black --stdin-filename $FILENAME --quiet -",
-				-- 	},
-			},
-			ruff = {
-				command = 'pwsh.exe -Command "uv run ruff check --fix --force-exclude --exit-zero --no-cache $FILENAME"',
-				-- args = {
-				-- 	"-Command",
-				-- 	"uv run ruff check --fix --force-exclude --exit-zero --no-cache --stdin-filename $FILENAME -",
-				-- },
-			},
-		}
-	else
 		return {}
+	else
+		return { "isort", "black", "ruff" }
 	end
 end
 
@@ -40,41 +18,23 @@ M.config = function()
 				"stylua",
 				-- "luaformatter",
 			},
-			python = { "isort", "black", "ruff" },
+			python = python_wsl_win_formatters(),
 			rust = { "rustfmt", lsp_format = "fallback" },
 			-- javascript = { "prettierd", "prettier", stop_after_first = true },
 		},
-		formatters = python_wsl_win_formatters(),
 	})
-	-- if vim.fn.getenv("IS_WSL_WIN") == "1" then
-	-- 	conform.formatters.ruff = {
-	-- 		-- inherit = false,
-	-- 		command = "pwsh.exe",
-	-- 		args = {
-	-- 			"-Command",
-	-- 			"uv run ruff check --fix --force-exclude --exit-zero --no-cache --stdin-filename $FILENAME -",
-	-- 		},
-	-- 	}
-	-- 	conform.formatters.black = {
-	-- 		-- inherit = false,
-	-- 		command = "pwsh.exe",
-	-- 		args = {
-	-- 			"-Command",
-	-- 			"uv run black --stdin-filename $FILENAME --quiet -",
-	-- 		},
-	-- 	}
-	-- 	conform.formatters.isort = {
-	-- 		-- inherit = false,
-	-- 		command = "",
-	-- 		args = {
-	-- 			"",
-	-- 		},
-	-- 	}
-	-- end
 	vim.api.nvim_create_autocmd("BufWritePre", {
 		pattern = "*",
 		callback = function(args)
 			require("conform").format({ bufnr = args.buf, timeout_ms = 500 })
+		end,
+	})
+	vim.api.nvim_create_autocmd("BufWritePre", {
+		pattern = "*.py",
+		callback = function(args)
+			print(args.buf)
+			print(args.buf)
+			print(args.buf)
 		end,
 	})
 end
