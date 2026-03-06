62d7e00c08efbdb820d2ddaffab750a6aef7f623
the-citto <alex.citterio@hotmail.co.uk>
2026-03-06 19:35:41 +0000

    map filetypes

--- lua/plugins/nvim-web-devicons.lua
+++ lua/plugins/nvim-web-devicons.lua
@@ -1,68 +1,70 @@
 local M = {
-    "nvim-tree/nvim-web-devicons",
+	"nvim-tree/nvim-web-devicons",
 }
 
-
-M.config = function ()
-    require"nvim-web-devicons".setup {
-        override_by_filename = {
-            ["requirements.in"] = {
-                icon = "",
-                color = "#2D4F67",
-                name = "Requirements"
-            },
-            ["requirements-tests.in"] = {
-                icon = "",
-                color = "#2D4F67",
-                name = "Requirements"
-            },
-            ["requirements-dev.in"] = {
-                icon = "",
-                color = "#2D4F67",
-                name = "Requirements"
-            },
-            ["requirements.txt"] = {
-                icon = "",
-                color = "#2D4F67",
-                name = "Requirements"
-            },
-            ["requirements-dev.txt"] = {
-                icon = "",
-                color = "#2D4F67",
-                name = "Requirements"
-            },
-            ["requirements-tests.txt"] = {
-                icon = "",
-                color = "#2D4F67",
-                name = "Requirements"
-            },
-            ["py.typed"] = {
-                icon = "",
-                color = "#2D4F67",
-                name = "py.typed"
-            },
-            [".python-version"] = {
-                icon = "",
-                color = "#2D4F67",
-                name = ".python-version"
-            },
-            [".gcloudignore"] = {
-                icon = "",
-                color = "#658594",
-                name = ".gcloudignore"
-            },
-            [".coveragerc"] = {
-                icon = "",
-                name = ".coveragerc",
-            },
-            [".flake8"] = {
-                icon = "",
-                name = ".flake8",
-            },
-        }
-    }
+M.config = function()
+	require("nvim-web-devicons").setup({
+		override_by_filename = {
+			["requirements.in"] = {
+				icon = "",
+				color = "#2D4F67",
+				name = "Requirements",
+			},
+			["requirements-tests.in"] = {
+				icon = "",
+				color = "#2D4F67",
+				name = "Requirements",
+			},
+			["requirements-dev.in"] = {
+				icon = "",
+				color = "#2D4F67",
+				name = "Requirements",
+			},
+			["requirements.txt"] = {
+				icon = "",
+				color = "#2D4F67",
+				name = "Requirements",
+			},
+			["requirements-dev.txt"] = {
+				icon = "",
+				color = "#2D4F67",
+				name = "Requirements",
+			},
+			["requirements-tests.txt"] = {
+				icon = "",
+				color = "#2D4F67",
+				name = "Requirements",
+			},
+			["py.typed"] = {
+				icon = "",
+				color = "#2D4F67",
+				name = "py.typed",
+			},
+			[".python-version"] = {
+				icon = "",
+				color = "#2D4F67",
+				name = ".python-version",
+			},
+			[".gcloudignore"] = {
+				icon = "",
+				color = "#658594",
+				name = ".gcloudignore",
+			},
+			[".coveragerc"] = {
+				icon = "",
+				name = ".coveragerc",
+			},
+			[".flake8"] = {
+				icon = "",
+				name = ".flake8",
+			},
+			["just.vars"] = {
+				icon = "",
+				color = "#658594",
+				name = "just.file",
+			},
+		},
+	})
 end
 
-
 return M
-
